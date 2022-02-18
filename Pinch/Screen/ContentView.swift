69ad3a8f8ab67f1dc76979e019f7f2d0758e47f6
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-19 01:30:52 +0530

    Initial Commit

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by saeem  on 19/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
