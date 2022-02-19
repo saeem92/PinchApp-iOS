d25394fadc19bb9e83531999cdf4bcca9cfdb243
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-20 01:34:40 +0530

    Added Image, Scale Effect and Double Tap Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    @State private var isAnimating: Bool = false // This will store the actual state of the animation we have created this to add animation in our app.
+    @State private var  imageScale: CGFloat = 1
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack{
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity((0.2)), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0) // This implicit animation asks swiftUI to apply a one second linear animation whenever the value of the isAnimating property changes
+                    .scaleEffect(imageScale)
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()){
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+                
+            }//: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating = true
+                }
+            })
+            //: NAVIGATION
+            
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
